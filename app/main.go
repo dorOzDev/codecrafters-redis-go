60a623f33e8a4340e0bcc91b4f408f0a45f9e469
dorOzDev <dorzry@gmail.com>
2025-05-06 16:22:44 +0300

    support multiple connections

--- app/main.go
+++ app/main.go
@@ -18,12 +18,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
-	handleConnection(conn)
 }
 
 func handleConnection(conn net.Conn) {
