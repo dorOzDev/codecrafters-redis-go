d4913dc029dd82c01e6e36684ee32bc635f89355
dorOzDev <dorzry@gmail.com>
2025-05-15 18:13:08 +0300

    remove code smell

--- app/utils.go
+++ app/utils.go
@@ -6,28 +6,6 @@ import (
 	"strings"
 )
 
-type CaseInsensitiveMap[T any] struct {
-	data map[string]T
-}
-
-func NewCaseInsensitiveMap[T any]() *CaseInsensitiveMap[T] {
-	return &CaseInsensitiveMap[T]{data: make(map[string]T)}
-}
-
-func (m *CaseInsensitiveMap[T]) Set(key string, value T) {
-	m.data[strings.ToLower(key)] = value
-}
-
-func (m *CaseInsensitiveMap[T]) Get(key string) (T, bool) {
-	value, exists := m.data[strings.ToLower(key)]
-	return value, exists
-}
-
-func (m *CaseInsensitiveMap[T]) Exists(key string) bool {
-	_, exists := m.data[strings.ToLower(key)]
-	return exists
-}
-
 func GetFlagValue(flagName string) (string, bool) {
 	fmt.Println("attempt to get flag: ", flagName)
 	if !strings.HasPrefix(flagName, "--") {
