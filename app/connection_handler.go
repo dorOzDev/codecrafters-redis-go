a81013a5edd043e7021aad2a61e70503bae86d07
dorOzDev <dorzry@gmail.com>
2025-06-03 17:57:34 +0300

    remove redundant code

--- app/connection_handler.go
+++ app/connection_handler.go
@@ -45,11 +45,6 @@ func (handler ReplicaConnectionHandler) HandleConnection() error {
 	if err != nil {
 		return err
 	}
-	for !handler.readyToServe.Load() {
-		log.Println("[REPLICA] Not ready yet, blocking client")
-		time.Sleep(10 * time.Millisecond)
-	}
-
 	acceptConnections(handler.listener)
 
 	return nil
